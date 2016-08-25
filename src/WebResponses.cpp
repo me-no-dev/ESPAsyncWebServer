116380849577d005364d10642fe0a41349bffba3
Me No Dev <me-no-dev@vdnsbg.com>
2016-08-25 22:15:17 +0300

    add some mime types and a commented implementation of etag for cache

--- src/WebResponses.cpp
+++ src/WebResponses.cpp
@@ -347,12 +347,17 @@ void AsyncFileResponse::_setContentType(String path){
   if (path.endsWith(".html")) _contentType = "text/html";
   else if (path.endsWith(".htm")) _contentType = "text/html";
   else if (path.endsWith(".css")) _contentType = "text/css";
+  else if (path.endsWith(".json")) _contentType = "text/json";
   else if (path.endsWith(".js")) _contentType = "application/javascript";
   else if (path.endsWith(".png")) _contentType = "image/png";
   else if (path.endsWith(".gif")) _contentType = "image/gif";
   else if (path.endsWith(".jpg")) _contentType = "image/jpeg";
   else if (path.endsWith(".ico")) _contentType = "image/x-icon";
   else if (path.endsWith(".svg")) _contentType = "image/svg+xml";
+  else if (path.endsWith(".eot")) _contentType = "font/eot";
+  else if (path.endsWith(".woff")) _contentType = "font/woff";
+  else if (path.endsWith(".woff2")) _contentType = "font/woff2";
+  else if (path.endsWith(".ttf")) _contentType = "font/ttf";
   else if (path.endsWith(".xml")) _contentType = "text/xml";
   else if (path.endsWith(".pdf")) _contentType = "application/pdf";
   else if (path.endsWith(".zip")) _contentType = "application/zip";
