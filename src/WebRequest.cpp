1082a7387564f82927bd88cb42b6346c7aa3a836
Alex Tikhonov <layet@yandex.ru>
2016-10-20 22:43:28 +0800

    fix & in param mistake
    urlDecode must be called only on param value after parsing params to array

--- src/WebRequest.cpp
+++ src/WebRequest.cpp
@@ -219,7 +219,7 @@ void AsyncWebServerRequest::_addGetParams(String params){
     if (equal < 0 || equal > end) equal = end;
     String name = params.substring(start, equal);
     String value = equal + 1 < end ? params.substring(equal + 1, end) : String();
-    _addParam(new AsyncWebParameter(name, value));
+    _addParam(new AsyncWebParameter(name, urlDecode(value)));
     start = end + 1;
   }
 }
@@ -248,7 +248,7 @@ bool AsyncWebServerRequest::_parseReqHead(){
     _method = HTTP_OPTIONS;
   }
 
-  u = urlDecode(u);
+  //u = urlDecode(u);
   String g = String();
   index = u.indexOf('?');
   if(index > 0){
