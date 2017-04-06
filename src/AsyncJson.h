120984e6d2c4ce3a6245878b4b1e5c7f940b82e6
Charles <hallard04@free.fr>
2017-04-06 10:10:59 +0200

    Added getSize() to know JSON buffer size (#151)
    * Added doc for ws.enable and ws.enabled
    
    * added getSize()

--- src/AsyncJson.h
+++ src/AsyncJson.h
@@ -72,6 +72,8 @@ class AsyncJsonResponse: public AsyncAbstractResponse {
       return _contentLength;
     }
 
+   size_t getSize() { return _jsonBuffer.size(); }
+
     size_t _fillBuffer(uint8_t *data, size_t len){
       ChunkPrint dest(data, _sentLength, len);
       _root.printTo( dest ) ;
