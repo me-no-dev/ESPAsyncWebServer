7865f396810b451df8e4af96e9d80b156860fdd8
Me No Dev <me-no-dev@users.noreply.github.com>
2019-09-21 12:33:26 +0300

    Update build-pio.sh

--- travis/build-pio.sh
+++ travis/build-pio.sh
@@ -5,20 +5,20 @@ BOARD=$1
 echo -e "travis_fold:start:install_pio"
 pip install -U platformio
 
-platformio lib install https://github.com/bblanchon/ArduinoJson.git
+python -m platformio lib install https://github.com/bblanchon/ArduinoJson.git
 
 case $BOARD in
 esp32dev)
-  platformio lib -g install https://github.com/me-no-dev/AsyncTCP.git
+  python -m platformio lib -g install https://github.com/me-no-dev/AsyncTCP.git
   ;;
 esp12e)
-  platformio lib -g install https://github.com/me-no-dev/ESPAsyncTCP.git || true
+  python -m platformio lib -g install https://github.com/me-no-dev/ESPAsyncTCP.git || true
   ;;
 esac
 echo -e "travis_fold:end:install_pio"
 
 echo -e "travis_fold:start:test_pio"
 for EXAMPLE in $PWD/examples/*/*.ino; do
-    platformio ci $EXAMPLE -l '.' -b $BOARD
+    python -m platformio ci $EXAMPLE -l '.' -b $BOARD
 done
 echo -e "travis_fold:end:test_pio"
