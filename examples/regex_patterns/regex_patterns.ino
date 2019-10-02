748023b53f589268d62be7c9fe7c61458aae79f2
Me No Dev <me-no-dev@users.noreply.github.com>
2019-10-02 14:27:25 +0300

    add support for esp8266

--- examples/regex_patterns/regex_patterns.ino
+++ examples/regex_patterns/regex_patterns.ino
@@ -6,8 +6,13 @@
 //
 
 #include <Arduino.h>
+#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
