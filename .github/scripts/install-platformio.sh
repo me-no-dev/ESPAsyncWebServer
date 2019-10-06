4ef078b1317e25074f30c2e3900b57e07f0290df
Bob Mooij <b.mooij@beeliners.com>
2019-10-06 22:40:04 +0200

    echo buildflags

--- .github/scripts/install-platformio.sh
+++ .github/scripts/install-platformio.sh
@@ -10,20 +10,21 @@ echo "PlatformIO has been installed"
 echo ""
 
 
-function build_pio_sketch(){ # build_pio_sketch <board> <path-to-ino>
-    if [ "$#" -lt 2 ]; then
+function build_pio_sketch(){ # build_pio_sketch <board> <path-to-ino> <build-flags>
+    if [ "$#" -lt 3 ]; then
         echo "ERROR: Illegal number of parameters"
-        echo "USAGE: build_pio_sketch <board> <path-to-ino>"
+        echo "USAGE: build_pio_sketch <board> <path-to-ino> <build-flags>"
         return 1
     fi
 
 	local board="$1"
 	local sketch="$2"
-    local defines="$3"
+    local buildFlags="$3"
 	local sketch_dir=$(dirname "$sketch")
 	echo ""
 	echo "Compiling '"$(basename "$sketch")"' ..."
-	python -m platformio ci -l '.' --board "$board" "$sketch_dir" --project-option="board_build.partitions = huge_app.csv" --project-option="build_flags=$defines"
+	echo "With flags '$buildFlags'"
+	python -m platformio ci -l '.' --board "$board" "$sketch_dir" --project-option="board_build.partitions = huge_app.csv" --project-option="build_flags=$buildFlags"
 }
 
 function count_sketches() # count_sketches <examples-path>
@@ -119,16 +120,17 @@ function build_pio_sketches() # build_pio_sketches <board> <examples-path> <chun
         || [ -f "$sketchdir/.test.skip" ]; then
             continue
         fi
-        local sketchDefines
+        local sketchBuildFlags
         while read line; do
-            sketchDefines="$sketchDefines -D$line"
+            sketchBuildFlags="$sketchBuildFlags -D$line"
         done < "$sketchdir/.test.defines"
+        echo "sketchBuildFlags: '$sketchBuildFlags'"
         sketchnum=$(($sketchnum + 1))
         if [ "$sketchnum" -le "$start_index" ] \
         || [ "$sketchnum" -gt "$end_index" ]; then
         	continue
         fi
-        build_pio_sketch "$board" "$sketch" "$sketchDefines"
+        build_pio_sketch "$board" "$sketch" "$sketchBuildFlags"
         local result=$?
         if [ $result -ne 0 ]; then
             return $result
