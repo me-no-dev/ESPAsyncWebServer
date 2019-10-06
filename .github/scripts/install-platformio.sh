220cbbf419700b8b7461e96d495707d3b82601a8
Bob Mooij <b.mooij@beeliners.com>
2019-10-06 22:19:34 +0200

    adding defines to build

--- .github/scripts/install-platformio.sh
+++ .github/scripts/install-platformio.sh
@@ -19,10 +19,11 @@ function build_pio_sketch(){ # build_pio_sketch <board> <path-to-ino>
 
 	local board="$1"
 	local sketch="$2"
+    local defines="$3"
 	local sketch_dir=$(dirname "$sketch")
 	echo ""
 	echo "Compiling '"$(basename "$sketch")"' ..."
-	python -m platformio ci -l '.' --board "$board" "$sketch_dir" --project-option="board_build.partitions = huge_app.csv"
+	python -m platformio ci -l '.' --board "$board" "$sketch_dir" --project-option="board_build.partitions = huge_app.csv" --project-option="build_flags=$defines"
 }
 
 function count_sketches() # count_sketches <examples-path>
@@ -118,12 +119,16 @@ function build_pio_sketches() # build_pio_sketches <board> <examples-path> <chun
         || [ -f "$sketchdir/.test.skip" ]; then
             continue
         fi
+        local sketchDefines
+        while read line; do
+            sketchDefines="$sketchDefines -D$line"
+        done < "$sketchdir/.test.defines"
         sketchnum=$(($sketchnum + 1))
         if [ "$sketchnum" -le "$start_index" ] \
         || [ "$sketchnum" -gt "$end_index" ]; then
         	continue
         fi
-        build_pio_sketch "$board" "$sketch"
+        build_pio_sketch "$board" "$sketch" "$sketchDefines"
         local result=$?
         if [ $result -ne 0 ]; then
             return $result
