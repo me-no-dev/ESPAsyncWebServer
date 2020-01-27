b4509cf06f93f2a51da71db72473e38676fb60ae
Ayush Sharma <asrocks5@gmail.com>
2020-01-28 03:16:43 +0530

    Added Websocket handshake handler

--- src/AsyncWebSocket.h
+++ src/AsyncWebSocket.h
@@ -237,6 +237,7 @@ class AsyncWebSocketClient {
     void _onData(void *pbuf, size_t plen);
 };
 
+typedef std::function<bool(AsyncWebServerRequest *request)> AwsHandshakeHandler;
 typedef std::function<void(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len)> AwsEventHandler;
 
 //WebServer Handler implementation that plays the role of a socket server
@@ -248,6 +249,7 @@ class AsyncWebSocket: public AsyncWebHandler {
     AsyncWebSocketClientLinkedList _clients;
     uint32_t _cNextId;
     AwsEventHandler _eventHandler;
+    AwsHandshakeHandler _handshakeHandler;
     bool _enabled;
     AsyncWebLock _lock;
 
@@ -314,6 +316,10 @@ class AsyncWebSocket: public AsyncWebHandler {
     //event listener
     void onEvent(AwsEventHandler handler){
       _eventHandler = handler;
+    }
+
+    void handleConnect(AwsHandshakeHandler handler){
+      _handshakeHandler = handler;
     }
 
     //system callbacks (do not call)
