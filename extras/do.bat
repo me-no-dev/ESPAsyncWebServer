f0987eb9ae3fc887e176ed7092908c26245ea66c
lorol <lorolouis@gmail.com>
2020-03-31 23:38:33 -0400

    Added tool to help embedding htm file to code with one click See in /extras

---
+++ extras/do.bat
@@ -0,0 +1,7 @@
+copy ..\src\edit.htm edit_src.htm
+call html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true --minify-js true -o edit.htm edit_src.htm
+"C:\Program Files\7-Zip\7z.exe" a -tgzip -mx9 edit.htm.gz edit.htm
+ehg edit.htm.gz
+copy edit.htm.gz.h ..\src\edit.htm.gz.h
+pause
+del edit.htm edit.htm.gz edit.htm.gz.h edit_src.htm
---
+++ extras/undo.bat
@@ -0,0 +1,4 @@
+copy ..\src\edit.htm.gz.h edit.htm.gz.h 
+rehg edit.htm.gz.h edit.htm.gz
+"C:\Program Files\7-Zip\7z.exe" e -tgzip edit.htm.gz
+pause
