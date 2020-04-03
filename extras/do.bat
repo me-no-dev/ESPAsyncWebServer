da8e271b869a9efd4b1c5d73f78d5f76387abc4e
lorol <lorolouis@gmail.com>
2020-04-03 16:29:02 -0400

    Added another tool to update acefull.js.gz from sources in addition to tools for embedding of edit.htm file Changed flags in do.bat to less aggressive edit.htm minifying w/o "Remove space between attributes" See: extras/update_ace.bat Updated examples/SmartSwitch/data/acefull.js.gz Updated Readme

--- extras/do.bat
+++ extras/do.bat
@@ -1,5 +1,5 @@
 copy ..\src\edit.htm edit_src.htm
-call html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true --minify-js true -o edit.htm edit_src.htm
+call html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes  --minify-css true --minify-js true -o edit.htm edit_src.htm
 "C:\Program Files\7-Zip\7z.exe" a -tgzip -mx9 edit.htm.gz edit.htm
 ehg edit.htm.gz
 copy edit.htm.gz.h ..\src\edit.htm.gz.h
---
+++ extras/update_ace.bat
@@ -0,0 +1,14 @@
+REM https://www.npmjs.com/package/github-files-fetcher   
+REM npm install -g github-files-fetcher
+REM fetcher --url=resource_url  --out=output_directory
+
+call fetcher --url="https://github.com/ajaxorg/ace-builds/blob/master/src-min-noconflict/ace.js" --out=tmp1
+call fetcher --url="https://github.com/ajaxorg/ace-builds/blob/master/src-min-noconflict/mode-html.js" --out=tmp1
+call fetcher --url="https://github.com/ajaxorg/ace-builds/blob/master/src-min-noconflict/theme-monokai.js" --out=tmp1
+
+cd tmp1
+type ace.js mode-html.js theme-monokai.js > acefull.js
+"C:\Program Files\7-Zip\7z.exe" a -tgzip -mx9 acefull.js.gz acefull.js
+REM update:
+copy acefull.js.gz ..\..\examples\SmartSwitch\data\acefull.js.gz
+pause
