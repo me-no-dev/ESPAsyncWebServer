735c00209d352b802bf87dd15a1f66ba1176947c
Drzony <drzony@gmail.com>
2020-11-06 14:19:28 +0100

    Fixed compilation when NO_GLOBAL_SPIFFS is defined

--- src/SPIFFSEditor.h
+++ src/SPIFFSEditor.h
@@ -6,14 +6,18 @@ class SPIFFSEditor: public AsyncWebHandler {
   private:
     fs::FS _fs;
     String _username;
-    String _password; 
+    String _password;
     bool _authenticated;
     uint32_t _startTime;
   public:
 #ifdef ESP32
     SPIFFSEditor(const fs::FS& fs, const String& username=String(), const String& password=String());
 #else
+#ifdef NO_GLOBAL_SPIFFS
+    SPIFFSEditor(const String& username, const String& password, const fs::FS& fs);
+#else
     SPIFFSEditor(const String& username=String(), const String& password=String(), const fs::FS& fs=SPIFFS);
+#endif
 #endif
     virtual bool canHandle(AsyncWebServerRequest *request) override final;
     virtual void handleRequest(AsyncWebServerRequest *request) override final;
