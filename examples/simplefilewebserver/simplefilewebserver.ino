3c0e7c7b6afb4ed8483e7960a52a4c20aeb37c89
Holger Lembke <lembke@gmail.com>
2021-08-28 14:09:38 +0200

    a simple file based web server

--- examples/simplefilewebserver/simplefilewebserver.ino
+++ examples/simplefilewebserver/simplefilewebserver.ino
@@ -1,1 +1,45 @@
-ini
+/*
+  This is an example of a simple file based web server. It can automatically serve gziped/.gz files.
+
+  Uses LittleFS.
+
+  Holger Lembke / lembke@gmail.com 2021
+*/
+
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <FS.h>
+#include <LittleFS.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "....";
+const char* password = "....";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  if (! LittleFS.begin(false)) { // change to true if you want to force formatting
+    Serial.println("LittleFS Mount Failed, no forced formatting.");
+  }
+
+  // this is it!
+  setupWebserver();
+}
+
+void loop() {
+  // empty!
+}
---
+++ examples/simplefilewebserver/webserver.ino
@@ -0,0 +1,78 @@
+// WARNING: This webserver has no access control. It servers everything from your ESP32 file system.
+// WARNING: This webserver has no access control. It servers everything from your ESP32 file system.
+AsyncWebServer * webserver = NULL;
+const word webserverServerPort = 80;
+const String webserverDefaultname = "/index.html";
+// WARNING: This webserver has no access control. It servers everything from your ESP32 file system.
+// WARNING: This webserver has no access control. It servers everything from your ESP32 file system.
+
+//*****************************************************************************************************
+void setupWebserver(void) {
+  webserver = new AsyncWebServer(webserverServerPort);
+
+  /* 
+     OK, here is the trick:
+        everything falls into the "not found" function. It will search for a matching
+        file, if found serve it and if not found call the real notfound function.
+  */
+  webserver->onNotFound(notFound);
+  webserver->begin();
+}
+
+//*****************************************************************************************************
+//*internals.
+//*****************************************************************************************************
+// 404
+void notFound404(AsyncWebServerRequest *request) {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += request->url();
+  message += "\nMethod: ";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += request->args();
+  message += "\n";
+  for (uint8_t i = 0; i < request->args(); i++) {
+    message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+  }
+  message += "\nHeaders: ";
+  message += request->headers();
+  message += "\n";
+  for (uint8_t i = 0; i < request->headers(); i++) {
+    message += " " + request->headerName(i) + ": " + request->header(i) + "\n";
+  }
+
+  // IE will mindesten 512 Byte, um es anzuzeigen, sonst interne IE-Seite....
+  while (message.length() < 512) {
+    message += "                         \n";
+  }
+  request->send(404, "text/plain", message);
+}
+
+//*****************************************************************************************************
+void notFound(AsyncWebServerRequest *request) {
+  String uri = request->url();
+
+  // default name
+  if (uri == "/") {
+    uri = webserverDefaultname;
+  }
+
+  // Are we allowd to send compressed data?
+  if (request->hasHeader("ACCEPT-ENCODING")) {
+    if (request->header("ACCEPT-ENCODING").indexOf("gzip") != -1) {
+      // gzip version exists?
+      if (LittleFS.exists(uri + ".gz"))  {
+        uri += ".gz";
+      }
+    }
+  }
+
+  if (LittleFS.exists(uri)) {
+    request->send(LittleFS, "/index.html", String(), false);
+  } else {
+    notFound404(request);
+  }
+
+  delay(1);
+}
