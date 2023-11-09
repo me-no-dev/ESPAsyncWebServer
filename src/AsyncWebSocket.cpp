ed5ab95ae914324590ee2fc9932fdf9beba789bf
Magnus <mp-se@users.noreply.github.com>
2023-11-09 19:24:04 +0100

    Fix cast for esp32c3

--- src/AsyncWebSocket.cpp
+++ src/AsyncWebSocket.cpp
@@ -829,7 +829,7 @@ void AsyncWebSocketClient::binary(AsyncWebSocketMessageBuffer * buffer)
 
 IPAddress AsyncWebSocketClient::remoteIP() {
     if(!_client) {
-        return IPAddress(0U);
+        return IPAddress(static_cast<uint32_t>(0U));
     }
     return _client->remoteIP();
 }
