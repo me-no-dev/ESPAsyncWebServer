e9c71ad7d80c92f194a70f299df22ab24b351126
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-07-17 19:18:39 +0200

    example update

--- examples/issues/Issue14/Issue14.ino
+++ examples/issues/Issue14/Issue14.ino
@@ -9,128 +9,84 @@
   #include <WebServer.h>
   #include <WiFi.h>
 #endif
-#include "ESPAsyncWebServer.h"
 
-#include <queue>
+#include "ESPAsyncWebServer.h"
 
 const char appWebPage[] PROGMEM = R"rawliteral(
 <body>
 <button id="button1" onclick="fetch('/button1');">Relay1</button>
 <script>
-
   const evtSource = new EventSource("/events");
-
   button1 = document.getElementById("button1");
-
-
   evtSource.addEventListener('state', (e) => {
       const data = JSON.parse(e.data);
       console.log('Event Source data: ', data);
-
       if (data.button1) {
           button1.style.backgroundColor = "green";
       }
       else {
           button1.style.backgroundColor = "red";
       }
-
   });
-
-
 </script>
 </body>
 )rawliteral";
 
-void sendState();
-
-// const char* ssid = "test_esp32";
-// const char* password = "123456789";
-
-//==========================================================
-
-// Настройки IP адреса
-// IPAddress local_IP(192, 168, 111, 111);
-// IPAddress gateway(192, 168, 111, 1);
-// IPAddress subnet(255, 255, 255, 0);
-
-// AsyncWebServer server(22222);
 AsyncWebServer server(80);
-
 AsyncEventSource events("/events");
 
-unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;
-const uint16_t interval = 500;
-
+const uint32_t interval = 1000;
 const int button1Pin = 4;
-bool button1 = false;
 
-char json[512];
+uint32_t lastSend = 0;
 
-void prepareJson() {
-  snprintf(json, 511, "{\"button1\":%d,\"1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij\":%ld}", button1, random(0, 999999999));
+void prepareJson(String& buffer) {
+  buffer.reserve(512);
+  buffer.concat("{\"button1\":");
+  buffer.concat(digitalRead(button1Pin) == LOW);
+  buffer.concat(",\"1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij1234567890abcdefghij\":");
+  buffer.concat(random(0, 999999999));
+  buffer.concat("}");
 }
 
-void handleRoot(AsyncWebServerRequest* request) {
-  request->send(200, "text/html", appWebPage);
-}
-
-void button1off() {
-  button1 = false;
-  digitalWrite(button1Pin, HIGH);
-}
-
-void button1on() {
-  digitalWrite(button1Pin, LOW);
-  button1 = true;
-}
-
-void sendState() {
-  prepareJson();
-  events.send(json, "state", millis());
-}
-
-//=========================================================
 void setup() {
-
   Serial.begin(115200);
+#if ARDUINO_USB_CDC_ON_BOOT
+  Serial.setTxTimeoutMs(0);
   delay(100);
+#else
+  while (!Serial)
+    yield();
+#endif
+
   randomSeed(micros());
 
   pinMode(button1Pin, OUTPUT);
-  button1off();
+  digitalWrite(button1Pin, HIGH);
 
-  // WiFi.softAPConfig(local_IP, gateway, subnet);
-  // WiFi.softAP(ssid, password, 11);
-  
   WiFi.softAP("esp-captive");
-  
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin("IoT");
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(1000);
-  //   Serial.println("Connecting to WiFi..");
-  // }
-  // Serial.println(WiFi.localIP());
 
-  // WiFi.setTxPower(WIFI_POWER_21dBm);
-  // esp_wifi_set_protocol(WIFI_IF_AP, WIFI_PROTOCOL_11B);
-
-  server.on("/", HTTP_GET, handleRoot);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(200, "text/html", appWebPage);
+  });
 
   server.on("/button1", HTTP_GET, [](AsyncWebServerRequest* request) {
     request->send(200, "text/plain", "OK");
-    if (button1) {
-      button1off();
-    } else {
-      button1on();
-    }
-    sendState();
+    digitalWrite(button1Pin, digitalRead(button1Pin) == LOW ? HIGH : LOW);
+
+    String buffer;
+    prepareJson(buffer);
+    ESP_LOGI("async_tcp", "Sending from handler...");
+    events.send(buffer.c_str(), "state", millis());
+    ESP_LOGI("async_tcp", "Sent from handler!");
   });
 
   events.onConnect([](AsyncEventSourceClient* client) {
-    prepareJson();
-    client->send(json, "state", millis(), 5000);
+    String buffer;
+    prepareJson(buffer);
+    ESP_LOGI("async_tcp", "Sending from onConnect...");
+    client->send(buffer.c_str(), "state", millis(), 5000);
+    ESP_LOGI("async_tcp", "Sent from onConnect!");
   });
 
   server.addHandler(&events);
@@ -140,10 +96,12 @@ void setup() {
 }
 
 void loop() {
-
-  currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    sendState();
+  if (millis() - lastSend >= interval) {
+    String buffer;
+    prepareJson(buffer);
+    ESP_LOGI("loop", "Sending...");
+    events.send(buffer.c_str(), "state", millis());
+    ESP_LOGI("loop", "Sent!");
+    lastSend = millis();
   }
 }
